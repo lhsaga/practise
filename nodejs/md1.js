77505c3e8a3a3bb242b149f5f3925badbdafdb04
Liang <liuliang@myscandic.com>
2020-09-27 01:10:35 +0300

    3

---
+++ nodejs/md1.js
@@ -0,0 +1,6 @@
+exports.add=function(a,b){
+    return a+b
+}
+exports.mult=function(a,b){
+    return a*b
+}
---
+++ nodejs/node.js
@@ -0,0 +1,11 @@
+console.log("test__info")
+
+
+var md= require("./md1")
+
+console.log(md.add(3,199))
+
+var fs=require("fs")
+
+console.log(fs)
+console.log(arguments.callee+"")
